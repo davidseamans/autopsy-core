2b98fb65d7ef7b136bd1cd06a94aa112eec4944f
David Seamans <david@davidseamans.com.au>
2026-04-10 18:30:29 +1000

    Create prevent_run_questions_update.sql

---
+++ supabase/functions/prevent_run_questions_update.sql
@@ -0,0 +1,12 @@
+-- =========================================
+-- FUNCTION: prevent_run_questions_update
+-- =========================================
+
+CREATE OR REPLACE FUNCTION public.prevent_run_questions_update()
+RETURNS trigger
+LANGUAGE plpgsql
+AS $function$
+begin
+  raise exception 'run_questions is immutable once created';
+end;
+$function$ language plpgsql;
